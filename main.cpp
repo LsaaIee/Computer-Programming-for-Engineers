44c1cbd1641abd7620ce10e0a76368531afb98c0
LsaaIee <107087441+LsaaIee@users.noreply.github.com>
2023-04-12 22:40:33 +0900

    reset main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,7 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-int main() {
-    int n;
-    string words[n];
-    
-    cin >> n;
-    
-    for (int i = 0; i < n; i++){
-        cin >> words[i];
-    }
-    
-    for (int j = 0; j < n; j++){
-        string temp;
-        if (words[j].length() > words[j++].length()){
-            temp = words[j];
-            words[j] = words[j++];
-            words[j++] = temp;
-        }
-        
-        else if (words[j].length() == words[j++].length()){
-            for 
-        }
-    }
-    
-    
-    
-    
+int main(){
+    //write codes
     return 0;
 }
